797160fbf06dd51be9665bb574f5a4932c710e59
Rafal Pysz <kuchasz1@o2.pl>
2017-01-21 06:15:58 +0100

    broken component

---
+++ src/client/components/login-screen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {css} from "glamor";
+
+export const add = (x, y) => {
+  return x + y;
+};
+
+export const LoginScreen = () => (
+  <div>
+      <div>DUPA</div>
+  </div>
+);
---
+++ src/client/components/login-screen/index.test.js
@@ -0,0 +1,7 @@
+import {add} from "./";
+
+describe("Add", () => {
+  test("2 + 2", () => {
+    expect(add(2, 2)).toEqual(4);
+  });
+});
