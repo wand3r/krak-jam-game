24c4e4745370d300e9ffa4a05a2ef9c3e6116dea
Wojciech Karas <wkaras89@gmail.com>
2017-01-21 09:12:33 +0100

    room wip

--- src/client/components/login-screen/index.jsx
+++ src/client/components/login-screen/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {css} from "glamor";
-import { Button } from 'react-toolbox/lib/button';
-
 
 export const add = (x, y) => {
   return x + y;
--- src/client/components/rooms/index.jsx
+++ src/client/components/rooms/index.jsx
@@ -5,9 +5,12 @@ import {processAction} from "../../../lefrex-js/action-processor";
 import {subscribeToEvent} from "../../../lefrex-js/event-aggregator";
 import * as roomActions from "../../../domain/room/actions";
 import * as roomEvents from "../../../domain/room/events";
-import {ModalContainer, ModalDialog} from 'react-modal-dialog'
 import { currentUser } from '../../userProfile'
 import FlatButton from 'material-ui/FlatButton';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 
 const SingleRoom = ({id, name, teams, join}) => {
     return (
@@ -43,45 +46,42 @@ export class RoomsView extends Component {
         height: "100%", 
       })}>
         <div {...css({
-          display: "flex", justifyContent: "space-around", 
-          height: "40px"
+          display: "flex", justifyContent: "space-between", 
+          height: "50px"
         })}>
           <input 
             {...css({height: "100%", flex: "1", fontSize: "1.5em", padding: "0 1%"})}
             placeholder="Search room name" 
             onChange={({target: {value}}) => this.setState({roomNameSearch: value}) } 
           />
-          <button
-            {...css({
-              width: "50px", height: "100%", 
-              fontSize: "1em", padding: "0 1%"
-            })}
-            onClick={() => this.setState({creatingRoom: true})} 
+          <div {...css({display: "flex", margin: "0em 2em", justifyContent: "center"})}>
+            <FloatingActionButton
+                onClick={() => this.setState({creatingRoom: true})} 
+            >
+                <ContentAdd />
+            </FloatingActionButton>
+          </div>
+          <Dialog
+            title="Create Room"
+            actions={[]}
+            modal={true}
+            open={creatingRoom}
+            onRequestClose={() => this.setState({creatingRoom: false})}
           >
-            +
-          </button>
-          {
-            creatingRoom &&
-            <ModalContainer onClose={() => this.setState({creatingRoom: false})}>
-                <ModalDialog onClose={() => this.setState({creatingRoom: false})}>
-                    <input 
-                        ref={c => c && c.focus()}
-                        placeholder="Room name"
-                        {...css({
-                            fontSize: "1.5em",
-                        })}
-                        onKeyUp={({keyCode, target: {value}}) => {
-                            if(keyCode === 13) { 
-                                createRoom(value)
-                                this.setState({creatingRoom: false})
-                            } else if (keyCode === 27) {
-                                this.setState({creatingRoom: false})
-                            }
-                        }}
-                    />
-                </ModalDialog>
-            </ModalContainer>
-          }
+             <TextField 
+                ref={c => c && c.focus()}
+                fullWidth={true}
+                floatingLabelText="Room name"
+                onKeyUp={({keyCode, target: {value}}) => {
+                    if(keyCode === 13) { 
+                        createRoom(value)
+                        this.setState({creatingRoom: false})
+                    } else if (keyCode === 27) {
+                        this.setState({creatingRoom: false})
+                    }
+                }}
+            />
+          </Dialog>
         </div>
         <div {...css({flex: "1"})}>
           <FlipMove>
