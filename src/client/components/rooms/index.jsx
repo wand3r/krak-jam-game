53cc527bb8124f118fa12ff01fdb44733cb3e9f5
Rafal Pysz <kuchasz1@o2.pl>
2017-01-21 10:44:10 +0100

    components fixes

--- src/client/components/rooms/index.jsx
+++ src/client/components/rooms/index.jsx
@@ -6,125 +6,146 @@ import {subscribeToEvent} from "../../../lefrex-js/event-aggregator";
 import * as roomActions from "../../../domain/room/actions";
 import * as roomEvents from "../../../domain/room/events";
 import {ModalContainer, ModalDialog} from 'react-modal-dialog'
-import { currentUser } from '../../userProfile'
-import FlatButton from 'material-ui/FlatButton';
+import {currentUser} from '../../userProfile'
+import {FontIcon, RaisedButton, Card, Subheader, TextField} from 'material-ui';
+import SocialPerson from 'material-ui/svg-icons/social/person';
+import ActionPanTool from 'material-ui/svg-icons/action/pan-tool';
 
 const SingleRoom = ({id, name, teams, join}) => {
+    const players = [...teams[0], ...teams[1]];
+    console.log(players);
     return (
-        <div
-            {...css({display: "flex", border: "blue 1px solid", padding: "1em", margin: "1em 0"})}
-        >
-            <div {...css({flex: 1})}>
-                {name}
+        <Card style={{backgroundColor: 'transparent'}}>
+            <div {...css({display: 'flex', alignItems: 'row', padding: '18px'})}>
+                <div {...css({flex: 1})}>
+                    <div {...css({fontSize: '24px', margin: '0px 0px 4px 0px'})}>{name}</div>
+                    <span {...css({fontSize: '12px'})}>{players.map((p, id) => (
+                        <span key={id}>{p}, </span>
+                    ))}</span>
+                </div>
+                <span {...css({display: 'flex', alignItems: 'center'})}>
+                    <SocialPerson style={{width: 48, height: 48}} color={'rgb(255, 235, 59)'}/>
+                    <span>{teams[0].length} vs {teams[1].length}</span>
+                    <RaisedButton
+                        style={{marginLeft: '18px'}}
+                        backgroundColor={'rgb(255, 235, 59)'}
+                        label="Join game"
+                        icon={<ActionPanTool/>}/>
+                </span>
             </div>
-            <div {...css({flex: 1})}>
-                {teams[0]} vs {teams[1]}
-            </div>
-            <FlatButton label="Join" primary={true} onClick={() => join(id)}/>
-        </div>
+        </Card>
     )
 };
 
 
 export class RoomsView extends Component {
-  state = {
-    roomNameSearch: "",
-    creatingRoom: false,
-  };
-  render() {
-    const { rooms, joinRoom, createRoom } = this.props
-    console.log(rooms)
-    const { roomNameSearch, creatingRoom } = this.state
-    const filteredRooms = rooms.filter(({name}) => name.includes(roomNameSearch))
-    return (
-      <div {...css({
-        display: "flex", flexDirection: "column",
-        flex: 1, 
-        height: "100%", 
-      })}>
-        <div {...css({
-          display: "flex", justifyContent: "space-around", 
-          height: "40px"
-        })}>
-          <input 
-            {...css({height: "100%", flex: "1", fontSize: "1.5em", padding: "0 1%"})}
-            placeholder="Search room name" 
-            onChange={({target: {value}}) => this.setState({roomNameSearch: value}) } 
-          />
-          <button
-            {...css({
-              width: "50px", height: "100%", 
-              fontSize: "1em", padding: "0 1%"
-            })}
-            onClick={() => this.setState({creatingRoom: true})} 
-          >
-            +
-          </button>
-          {
-            creatingRoom &&
-            <ModalContainer onClose={() => this.setState({creatingRoom: false})}>
-                <ModalDialog onClose={() => this.setState({creatingRoom: false})}>
-                    <input 
-                        ref={c => c && c.focus()}
-                        placeholder="Room name"
-                        {...css({
-                            fontSize: "1.5em",
-                        })}
-                        onKeyUp={({keyCode, target: {value}}) => {
-                            if(keyCode === 13) { 
-                                createRoom(value)
-                                this.setState({creatingRoom: false})
-                            } else if (keyCode === 27) {
-                                this.setState({creatingRoom: false})
-                            }
-                        }}
+    state = {
+        roomNameSearch: "",
+        creatingRoom: false,
+        newRoomName: ""
+    };
+
+    render() {
+        const {rooms, joinRoom, createRoom} = this.props;
+        const {roomNameSearch, creatingRoom} = this.state;
+        const filteredRooms = rooms.filter(({name}) => name.includes(roomNameSearch));
+        return (
+            <div {...css({
+                display: "flex", flexDirection: "column",
+                backgroundColor: '#EEE',
+                flex: 1,
+                height: "100%",
+            })}>
+                <div {...css({
+                    display: "flex", justifyContent: "space-around",
+                    height: "40px"
+                })}>
+                    <input
+                        {...css({height: "100%", minWidth: '800px', flex: "1", fontSize: "1.5em", padding: "0 1%"})}
+                        placeholder="Search room name"
+                        onChange={({target: {value}}) => this.setState({roomNameSearch: value}) }
                     />
-                </ModalDialog>
-            </ModalContainer>
-          }
-        </div>
-        <div {...css({flex: "1"})}>
-          <FlipMove>
-            {filteredRooms.map((room, index) => (
-              <div key={room.id}>
-                <SingleRoom 
-                  {...room}
-                  join={joinRoom}
-                />
-              </div>
-            ))}
-          </FlipMove>
-        </div>
-        <div {...css({display: "flex", justifyContent: "space-around", height: "40px"})}>
-          
-        </div>
-      </div>
-    )
-  }
+                    <button
+                        {...css({
+                            width: "50px", height: "100%",
+                            fontSize: "1em", padding: "0 1%"
+                        })}
+                        onClick={() => this.setState({creatingRoom: true})}
+                    >
+                        +
+                    </button>
+                    {
+                        creatingRoom &&
+                        <ModalContainer onClose={() => this.setState({creatingRoom: false})}>
+                            <ModalDialog style={{borderRadius: '0px'}}
+                                         onClose={() => this.setState({creatingRoom: false})}>
+                                <Card>
+                                    <span {...css({fontSize: '24px', margin: '18px', color: '#444', padding: '12px'})}>Creating new room</span>
+                                    <div {...css({padding: '18px'})}>
+                                        <TextField onChange={({target:{value}}) => this.setState({newRoomName: value})}
+                                                   hintText="Type your room name"
+                                                   style={{margin: '10px'}}/>
+                                        <RaisedButton
+                                            label="Create room"
+                                            onClick={() => {
+                                                createRoom(this.state.newRoomName);
+                                                this.setState({creatingRoom: false});
+                                            }
+                                            }
+                                            primary={true}/>
+                                    </div>
+                                </Card>
+                            </ModalDialog>
+                        </ModalContainer>
+                    }
+                </div>
+                <div {...css({flex: "1"})}>
+                    <FlipMove>
+                        {filteredRooms.map((room, index) => (
+                            <div key={room.id}>
+                                <SingleRoom
+                                    {...room}
+                                    join={joinRoom}
+                                />
+                            </div>
+                        ))}
+                    </FlipMove>
+                </div>
+                <div {...css({display: "flex", justifyContent: "space-around", height: "40px"})}>
+
+                </div>
+            </div>
+        )
+    }
 }
 
 export class Rooms extends Component {
     state = {
         rooms: []
     };
+
     componentDidMount() {
         this.fetchRoomsList();
         subscribeToEvent(
-            roomEvents.room.roomCreatedEvent, 
-            (room) =>  { 
+            roomEvents.room.roomCreatedEvent,
+            (room) => {
                 this.fetchRoomsList()
             }
         )
     }
+
     componentWillUnmount() {
 
     }
+
     fetchRoomsList = () => {
         processAction(roomActions.createGetRoomsAction())
             .then((rooms) => {
                 this.setState({rooms});
             })
-            .catch(x => { throw new Error(x) });
+            .catch(x => {
+                throw new Error(x)
+            });
     }
     createRoom = (name) => {
         processAction(roomActions.createCreateRoomAction(
@@ -135,13 +156,14 @@ export class Rooms extends Component {
     joinRoom = (roomId) => {
 
     }
+
     render() {
-        const { rooms } = this.state
+        const {rooms} = this.state
         return (
-            <RoomsView 
-                rooms={rooms} 
+            <RoomsView
+                rooms={rooms}
                 createRoom={this.createRoom}
-                joinRoom={this.joinRoom} 
+                joinRoom={this.joinRoom}
             />
         )
     }
--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
-css.global("html, body", {height: "100%", margin: "0"});
+css.global("html, body", {overflowY: 'scroll', height: "100%", margin: "0", backgroundSize: 'contain', backgroundImage:'url("https://farm3.staticflickr.com/2831/12657603295_fd3954a882_b.jpg")'});
 css.global("#app", {display: "flex", alignItems:'center', justifyContent:'center', minHeight: "100%", width: "100%"});
 css.global("*", {boxSizing: "border-box"});
 
