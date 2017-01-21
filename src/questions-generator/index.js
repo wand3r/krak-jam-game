40ae6a6f394f87f5c0bcde93309e27f6bedc5a8f
Rafal Pysz <kuchasz1@o2.pl>
2017-01-21 05:48:58 +0100

    improved questions crawler

--- src/questions-generator/index.js
+++ src/questions-generator/index.js
@@ -5,17 +5,31 @@ var results = [];
 
 var downloaded = 0;
 
-for (var i = 0; i < 100; i++) {
+for (var i = 0; i < 200; i++) {
     request('https://opentdb.com/api.php?amount=50', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var _body = JSON.parse(body);
             results = [...results, ..._body.results];
             downloaded++;
-            if (downloaded === 99) {
+            if (downloaded === 199) {
                 results = results.filter(r => r.type === 'multiple');
 
+                var flags2 = {};
+                var categories = results.map(r => r.category);
+                var distinctCategories = categories.filter(function(cat){
+                    if (flags2[cat]) {
+                        return false;
+                    }
+                    flags2[cat] = true;
+                    return true;
+                });
+                distinctCategories = distinctCategories.map((dc, idx) => ({
+                    name: dc,
+                    id: idx
+                }));
+
                 var flags = {};
-                var newResults = results.filter(function(entry) {
+                var distinctQuestions = results.filter(function(entry) {
                     if (flags[entry.question]) {
                         return false;
                     }
@@ -23,7 +37,15 @@ for (var i = 0; i < 100; i++) {
                     return true;
                 });
 
-                fs.writeFileSync('./questions.json', JSON.stringify(newResults));
+                distinctQuestions = distinctQuestions.map(dq => ({
+                    question: dq.question,
+                    categoryId: distinctCategories.find(dc => dc.name === dq.category).id,
+                    correctAnswer: dq['correct_answer'],
+                    incorrectAnswers: dq['incorrect_answers']
+                }));
+
+                fs.writeFileSync('./questions.json', JSON.stringify(distinctQuestions));
+                fs.writeFileSync('./questions-categories.json', JSON.stringify(distinctCategories));
             }
         }
     });
